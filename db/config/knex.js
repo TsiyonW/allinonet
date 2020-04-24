0859b5c4a610cb26d7504456d84710c1aa0e7d3b
= <=>
2020-04-24 08:45:31 +0300

    (fix)knex config file fixed

--- db/config/knex.js
+++ db/config/knex.js
@@ -1,5 +1,5 @@
 var environment = process.env.NODE_ENV || 'development'
-var config = require('./knexfile.js')[environment]
+var config = require('../../knexfile')[environment]
 
 module.exports = require('knex')(config)
 
--- knexfile.js
+++ knexfile.js
@@ -10,10 +10,10 @@ module.exports = {
       database:'postgres'
     },
     migrations:{
-      directory: __dirname + '../../migrations'
+      directory: __dirname + './db/migrations'
     },
     seeds:{
-      directory: __dirname + '../../seeds'
+      directory: __dirname + './db/seeds'
     }
   },
 
